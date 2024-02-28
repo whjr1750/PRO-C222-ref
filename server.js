5aebbc6b7d3d66fcc6a137fa32016ce8d1c7d0ad
whjr1750 <91913630+whjr1750@users.noreply.github.com>
2024-02-28 17:55:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: '',
-        pass: '',
+        user: 'ankit.marathe@whitehatjr.com',
+        pass: 'ezzd qgfe uzot hrcb',
     },
     secure: true,
 });
@@ -44,7 +44,7 @@ app.post("/send-mail", (req, res) => {
     const to = req.body.to;
     const url = req.body.url;
     const mailData = {
-        from: "apoorv.goyal@whitehatjr.com",
+        from: "ankit.marathe@whitehatjr.com",
         to: to,
         subject: "Join the video chat with me!",
         html: `<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
